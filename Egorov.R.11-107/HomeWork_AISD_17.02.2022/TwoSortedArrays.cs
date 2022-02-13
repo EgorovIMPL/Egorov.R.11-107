5f16020416d60b00646724b4f49001f581336991
EgorovIMPL <egorov.r.684017@gmail.com>
2022-02-13 18:02:07 +0300

    .

---
+++ Egorov.R.11-107/HomeWork_AISD_17.02.2022/TwoSortedArrays.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Egorov.R._11_107.HomeWork_AISD_17._02._2022
+{
+    public class TwoSortedArrays
+    {
+        public static int[] Start()
+        {
+            int[] arr1= new int[1];
+            int[] arr2 = new int[1];
+            using (StreamReader sr = new StreamReader(Console.ReadLine()))
+            {
+                arr1 = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+                arr2 = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            }
+            int[] sorted_array = new int[arr1.Length + arr2.Length];
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i < arr1.Length + arr2.Length; i++)
+            {
+                if (count1 < arr1.Length && count2 < arr2.Length && arr1[count1] <= arr2[count2])
+                {
+                    sorted_array[i] = arr1[count1];
+                    count1++;
+                }
+                else if (count1 < arr1.Length && count2 < arr2.Length && arr1[count1] >= arr2[count2])
+                {
+                    sorted_array[i] = arr2[count2];
+                    count2++;
+                }
+                else if(count1 >= arr1.Length)
+                {
+                    sorted_array[i] = arr2[count2];
+                    count2++;
+                }
+                else if(count2 >= arr2.Length)
+                {
+                    sorted_array[i] = arr1[count1];
+                    count1++;
+                }
+            }
+            return sorted_array;
+        }
+        public static void Print(int[] array)
+        {
+            foreach (var el in array)
+            {
+                Console.Write(el + " ");
+            }
+        }
+    }
+}
--- Egorov.R.11-107/Program.cs
+++ Egorov.R.11-107/Program.cs
@@ -4,6 +4,7 @@ using Egorov.R._11_107.ClassWork;
 using Egorov.R._11_107.ControlWork_15._12;
 using Egorov.R._11_107.HomeWork_22._12._2021;
 using Egorov.R._11_107.HomeWork_8._12._2021;
+using Egorov.R._11_107.HomeWork_AISD_17._02._2022;
 
 namespace Egorov.R._11_107
 {
@@ -11,7 +12,7 @@ namespace Egorov.R._11_107
     {
         static void Main(string[] args)
         {
-            Exam.TimeTable();
+            TwoSortedArrays.Print(TwoSortedArrays.Start());
         }
     }
 }
