028d54d6546ad2e0c3feae4656304d4d880bb43d
EgorovIMPL <egorov.r.684017@gmail.com>
2022-02-24 15:25:38 +0300

    .

---
+++ Egorov.R.11-107/HomeWork_ASD_24.02.2022/Permutation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Egorov.R._11_107.HomeWork_ASD_24._02._2022
+{
+    public class Permutation
+    {
+        public static void GetAllPermutations(int[] array,ref int[] result, int value = 0)
+        {
+            if (array.Length == 0)
+            {
+                int[] swap = new int[result.Length + 1];
+                for (int i = 0; i < result.Length; i++)
+                    swap[i] = result[i];
+                swap[result.Length] = value;
+                result = swap;
+                return;
+            }
+            for (int j = 0; j < array.Length; j++)
+            {
+                int[] newArray = new int[array.Length - 1];
+                int count = 0;
+                for (int g = 0; g < array.Length; g++)
+                    if (j != g)
+                    {
+                        newArray[count] = array[g];
+                        count++;
+                    }
+                GetAllPermutations(newArray,ref result, value * 10 + array[j]);
+            }
+        }
+        public static void TwoNumbersSum(int a, int b, int c)
+        {
+            int[] result1 = new int[0];
+            int[] result2 = new int[0];
+            int[] arrayA = IntToArray(a);
+            int[] arrayB = IntToArray(b);
+            GetAllPermutations(arrayA,ref result1);
+            GetAllPermutations(arrayB, ref result2);
+            foreach (var el1 in result1)
+            {
+                foreach (var el2 in result2)
+                {
+                    if (el1 + el2 == c)
+                    {
+                        Console.WriteLine("YES");
+                        Console.WriteLine(el1 + " " + el2);
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("NO");
+        }
+
+        public static int[] IntToArray(int number)
+        {
+            int[] array = new int[Convert.ToString(number).Length];
+            int count = 0;
+            while (number > 0)
+            {
+                array[count] = number % 10;
+                number /= 10;
+                count++;
+            }
+            return array;
+        }
+    }
+}
--- Egorov.R.11-107/Program.cs
+++ Egorov.R.11-107/Program.cs
@@ -5,6 +5,7 @@ using Egorov.R._11_107.ControlWork_15._12;
 using Egorov.R._11_107.HomeWork_22._12._2021;
 using Egorov.R._11_107.HomeWork_8._12._2021;
 using Egorov.R._11_107.HomeWork_AISD_17._02._2022;
+using Egorov.R._11_107.HomeWork_ASD_24._02._2022;
 using Egorov.R._11_107.HomeWork_INF_21._02._2022;
 
 namespace Egorov.R._11_107
@@ -13,14 +14,13 @@ namespace Egorov.R._11_107
     {
         static void Main(string[] args)
         {
-            var list = new CustomList();
-            list.Add(1);
-            list.Add(2);
-            list.Add(-3);
-            list.AddToHead(2);
-            list.Add(10, 1);
-            list.AddBeforeAndAfterK(2,3);
-            list.WriteToConsole();
+            Permutation.TwoNumbersSum(12, 13, 52);
+        }
+
+        public static void Print(int[] array)
+        {
+            foreach (var el in array)
+                Console.WriteLine(el);
         }
     }
 }
