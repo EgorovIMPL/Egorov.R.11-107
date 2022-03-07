67dc9917e1005e9f84ea1bea62feb556ed73a93e
EgorovIMPL <egorov.r.684017@gmail.com>
2022-03-07 10:40:41 +0300

    .

---
+++ Egorov.R.11-107/HomeWork_INF_07.03.2022/CustomArrayCollection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Egorov.R._11_107.HomeWork_INF_07._03._2022
+{
+    public class CustomArrayCollection<T> : ICustomCollection<T>
+    {
+        private T[] array;
+
+        public CustomArrayCollection() 
+        {
+            array = new T[0];
+        }
+
+        public CustomArrayCollection(T el)
+        {
+            array = new T[1] { el };
+        }
+
+        public CustomArrayCollection(T[] array) 
+        {
+            this.array = array;
+        }
+
+        public void Add(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(T[] elems)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll(T elem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Size()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Egorov.R.11-107/HomeWork_INF_07.03.2022/ICustomCollection.cs
@@ -0,0 +1,51 @@
+﻿namespace Egorov.R._11_107.HomeWork_INF_07._03._2022
+{
+    public interface ICustomCollection<T>
+    {
+        int Size();
+        
+        bool isEmpty();
+        
+        bool Contains(T elem);
+        
+        void Add(T elem);
+
+        
+        void AddRange(T[] elems);
+
+        /// <summary>
+        /// Удаление элемента со значением
+        /// </summary>
+        void Remove(T elem);
+
+        /// <summary>
+        /// Удаляет все элементы со значением
+        /// </summary>
+        void RemoveAll(T elem);
+
+        /// <summary>
+        /// Удаление элемента на позиции с номером 
+        /// </summary>
+        void RemoveAt(int index);
+
+        /// <summary>
+        /// Очищение коллекции
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Переворачивает список
+        /// </summary>
+        void Reverse();
+
+        /// <summary>
+        /// Вставка элемента на конкретную позицию
+        /// </summary>
+        void Insert(int index, T elem);
+
+        /// <summary>
+        /// Возвращает позицию элемента
+        /// </summary>
+        int IndexOf(T elem);
+    }
+}
