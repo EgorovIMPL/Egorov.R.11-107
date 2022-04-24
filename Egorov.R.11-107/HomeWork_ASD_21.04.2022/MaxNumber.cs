64e253de63e94f859bad81f3c7316315963a303c
EgorovIMPL <egorov.r.684017@gmail.com>
2022-04-24 18:27:44 +0300

    .

---
+++ Egorov.R.11-107/HomeWork_ASD_21.04.2022/MaxNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Egorov.R._11_107.HomeWork_ASD_21._04._2022
+{
+    public class MaxNumber
+    {
+        public int Max(int[] array)
+        {
+            int[] newArray = new int[0];
+            if (array.Length == 2)
+            {
+                if (array[0] > array[1])
+                    return array[0];
+                return array[1];
+            }
+            else if(array.Length == 1)
+            {
+                return array[0];
+            }
+            else
+            {
+                newArray = newArray.Append(Max(Cut(array, 0, array.Length / 2))).ToArray();
+                newArray = newArray.Append(Max(Cut(array, array.Length / 2, array.Length))).ToArray();
+            }
+            return Max(newArray);
+        }
+        private int[] Cut(int[] array, int a, int b)
+        {
+            int[] newArray = new int[0];
+            for (int i = a; i < b; i++)
+            {
+                newArray = newArray.Append(array[i]).ToArray();
+            }
+
+            return newArray;
+        }
+    }
+}
--- Egorov.R.11-107/Program.cs
+++ Egorov.R.11-107/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Egorov.R._11_107.HomeWork_ASD_21._04._2022;
 using Egorov.R._11_107.HomeWork_INF_04._04._2022;
 using Egorov.R._11_107.HomeWork_INF_18._04._2022;
 
@@ -8,14 +9,8 @@ namespace Egorov.R._11_107
     {
         static void Main(string[] args)
         {
-            AvlTree<string> tree = new AvlTree<string>("keyboard", 8);
-            tree.Add("ght",3);
-            tree.Add("ert",14);
-            tree.Add("why",12);
-            tree.Add("try",15);
-            tree.Add("because",17);
-            tree.PrintDepth();
-            
+            MaxNumber mx = new MaxNumber();
+            Console.WriteLine(mx.Max(new[] {17, 4, 25, 29, 3, 111, 13, 60,36,12}));
         }
     }
 }
