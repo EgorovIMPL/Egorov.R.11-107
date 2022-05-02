9aae209757569b11d73d96cd3c022e9215b56dc8
EgorovIMPL <egorov.r.684017@gmail.com>
2022-05-02 11:07:55 +0300

    .

---
+++ Egorov.R.11-107/HomeWork_INF_25.04.2022/PowerStation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Egorov.R._11_107.HomeWork_INF_25._04._2022
+{
+    public class PowerStation
+    {
+        public delegate void Danger();
+        public event Danger Start;
+        public void Run()
+        {
+            int t = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine($"Температура реактора: {t}");
+                if (i % 3 == 0)
+                    t += 20;
+                if (t == 300)
+                {
+                    Start();
+                    t = 0;
+                }
+            }
+        }
+    }
+
+    public class MCHS
+    {
+        public void Troubleshooting()
+        {
+            Console.WriteLine("Устранение ЧП");
+        }
+    }
+
+    public class FireStation
+    {
+        public void Extenguishing()
+        {
+            Console.WriteLine("Тушим пожар");
+        }
+    }
+}
--- Egorov.R.11-107/Program.cs
+++ Egorov.R.11-107/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Reflection;
 using Egorov.R._11_107.HomeWork_ASD_21._04._2022;
 using Egorov.R._11_107.HomeWork_INF_04._04._2022;
 using Egorov.R._11_107.HomeWork_INF_18._04._2022;
+using Egorov.R._11_107.HomeWork_INF_25._04._2022;
 
 namespace Egorov.R._11_107
 {
@@ -9,8 +11,12 @@ namespace Egorov.R._11_107
     {
         static void Main(string[] args)
         {
-            MaxNumber mx = new MaxNumber();
-            Console.WriteLine(mx.Max(new[] {17, 4, 25, 29, 3, 111, 13, 60,36,12}));
+            PowerStation ps = new PowerStation();
+            FireStation fs = new FireStation();
+            MCHS mchs = new MCHS();
+            ps.Start += fs.Extenguishing;
+            ps.Start += mchs.Troubleshooting;
+            ps.Run();
         }
     }
 }
